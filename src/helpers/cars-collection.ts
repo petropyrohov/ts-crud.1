3182039e82a93f6eb6ceb2af9a1c38f942f5b25f
petropyrohove <petroprodaktion@gmail.com>
2023-01-26 18:47:22 +0200

    feat (2)crited Constructor

--- src/helpers/cars-collection.ts
+++ src/helpers/cars-collection.ts
@@ -1,9 +1,24 @@
-type CarsCollectionProps = {
+import type Brand from "../types/brand";
+import type Car from "../types/car";
+import type Model from "../types/model";
 
+type CarsCollectionProps = {
+model:Model[],
+car: Car[],
+brand:Brand[],
 };
 
 class CarsCollection {
+ private  model: Model[];
+ private  car:Car[];
+ private brand :Brand[];
 
+ constructor({model,car,brand}:CarsCollection) {
+    this.model = JSON.parse(JSON.stringify(model));
+    this.brand = JSON.parse(JSON.stringify(brand));
+    this.car = JSON.parse(JSON.stringify(car));
+ 
+ }
 }
 
 export default CarsCollection;
